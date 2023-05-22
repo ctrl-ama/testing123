4f0f74602aac11cd6c2806053fd24a8d91f994d1
Amayindi Yerima <amayindi.yerima@binghamuni.edu.ng>
2023-05-22 18:19:39 +0100

    recent commit

---
+++ src/testinggg.java
@@ -0,0 +1,20 @@
+public class testinggg {
+    // Create a savings account
+    SavingsAccount savingsAccount = new SavingsAccount("SA1234", 5000);
+savingsAccount.displayBalance();  // Output: Account Number: SA1234, Balance: 5000
+
+savingsAccount.deposit(2000);
+savingsAccount.displayBalance();  // Output: Account Number: SA1234, Balance: 7000
+
+savingsAccount.withdraw(3000);
+savingsAccount.displayBalance();  // Output: Account Number: SA1234, Balance: 4000
+
+    // Create a current account
+    CurrentAccount currentAccount = new CurrentAccount("CA5678", 10000);
+currentAccount.displayBalance();  // Output: Account Number: CA5678, Balance: 10000
+
+currentAccount.withdraw(12000);  // Output: Insufficient funds
+currentAccount.withdraw(5000);
+currentAccount.displayBalance();  // Output: Account Number: CA5678, Balance: 5000
+
+}
